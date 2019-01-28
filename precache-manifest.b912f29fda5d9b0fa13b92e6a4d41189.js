f9f4d9915550b72089a6ee8587987b6beedbcfc0
Maira Vianey Jimenez Ledesma <mvjl@Mairas-MacBook-Pro.local>
2019-01-28 02:37:10 -0800

    Updates

---
+++ precache-manifest.b912f29fda5d9b0fa13b92e6a4d41189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14632635fc59752ffbde",
+    "url": "/Clicky-Game/static/css/main.0f87b088.chunk.css"
+  },
+  {
+    "revision": "14632635fc59752ffbde",
+    "url": "/Clicky-Game/static/js/main.14632635.chunk.js"
+  },
+  {
+    "revision": "405fe43fb4b888bea6c1",
+    "url": "/Clicky-Game/static/js/1.405fe43f.chunk.js"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "feecf27f7c4862b4b152dde9c6f89687",
+    "url": "/Clicky-Game/index.html"
+  }
+];
