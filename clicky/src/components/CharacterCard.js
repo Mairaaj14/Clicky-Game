6e7548ee0819bc5104d3263c6435ccdb4b36152f
Maira Vianey Jimenez Ledesma <mvjl@Mairas-MacBook-Pro.local>
2019-01-27 23:26:10 -0800

    character.json

---
+++ clicky/src/components/CharacterCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./CharacterCard.css";
+
+function CharacterCard(props) {
+    return (
+        <div>
+            <div>
+               <img alt={props.name} 
+            </div>
+        </div>
+    )
+}
