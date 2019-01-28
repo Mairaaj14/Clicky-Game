1faba26b7f76027322e6d03bdd96f295fd227ad3
Maira Vianey Jimenez Ledesma <mvjl@Mairas-MacBook-Pro.local>
2019-01-27 23:53:24 -0800

    header

---
+++ clicky/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css";
+
+function Header(props) {
+    return (
+        <header className="header"><h1>Dragon Ball Z's Clicky Game</h1>
+        <h2>Click on an image to start earning points, but don't click an image more than ONCE!</h2>
+        
+        </header>
+    )
+}
+export default Header;
