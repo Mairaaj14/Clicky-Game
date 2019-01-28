95df70688ee93add8266af3a835e3385f8b85fe1
Maira Vianey Jimenez Ledesma <mvjl@Mairas-MacBook-Pro.local>
2019-01-27 22:29:56 -0800

    app.js

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,28 +1,92 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Wrapper from "./components/Wrapper";
+import CharacterCard from "./components/CharacterCard";
+import Header from "./components/Header";
+import characters from "./characters.json";
+import Title from "./components/Title";
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  //Setting the state 
+  state = {
+    characters,
+    clicked: [],
+    currentScore: 0,
+    topScore: 0,
+    message: "",
+    headTitle: "Click an Image to Start!"
+  
+  };
+// Set the cards to shuffle
+  shuffleCards = id => {
+    const charactersRandom = arr => arr.sort(() => Math.random() - 0.5)
+
+    const characters = charactersRandom(this.state.characters);
+
+    const newScore = this.state.clicked.includes(id) ? 0 : this.state.currentScore + 1
+
+    const clickedReset = newScore === 0 ? [] : [...this.state.clicked, id]
+
+    const newTopScore = this.state.topScore < newScore ? newScore : this.state.topScore
+
+    this.setState({
+      characters,
+      clicked: clickedReset,
+      currentScore: newScore,
+      topScore: newTopScore
+    });
+
+    if (newScore  === 0) {
+      this.setState({
+        headTitle: "Guessed Incorrectly! Play Again!",
+      })
+
+    } else {
+      this.setState({
+        headTitle: "You guessed Correctly! Keep Clicking! "
+      })
+    }
+
   }
-}
+//   componentDidMount() {
+//     this.setState({characters: this.shuffle(this.state.characters)})
+//   };
+// // Set the cards to shuffle
+//   shuffle = data => {
+//     for (let i = data.length - 1; i > 0; i --) {
+//       const j = Math.floor(Math.random() * (i + 1));
+//       [data[i], data[j]] = [data[j], data[i]];
+//     }
+//     return data;
+  };
+
+  render() 
+  {
+    return (
+      <Wrapper>
+        <Title
+        currentScore={this.state.currentScore}
+        topScore={this.state.topScore}
+        headTitle={this.state.headTitle}
+        ></Title>
+        <Header></Header>
+        <Wrapper className = "container">
+        {this.state.characters.map(character => (
+          <CharacterCard
+          shuffleCards={this.shuffleCards}
+          id={character.id}
+          key={character.id}
+          name={character.name}
+          image={character.image}
+          currentScore={this.state.currentScore}
+          />
+        ))}
+
+          </Wrapper>
+      </Wrapper>
+    )
+  }
+
 
 export default App;
